5c155ffde650a1b9ddd989c8c0cc76d3a4393a4a
WhiskeySmashPaul <96494567+WhiskeySmashPaul@users.noreply.github.com>
2021-12-29 10:10:22 -0700

    Update AD_Lockout_Locator.ps1

--- AD_Lockout_Locator.ps1
+++ AD_Lockout_Locator.ps1
@@ -1,31 +1,44 @@
-  #Obtain information to search for from user
-  $UserName = Read-Host -Prompt 'Please enter username'
-  $DaysToSearch = Read-Host -Prompt 'Please enter how many days to search prior to today'
-  
-  #Gets a list of domain controllers to search through each DC
-  $ComputerName = (Get-ADDomainController -Filter * |  Select-Object -ExpandProperty Name)
+#region Variables - User Input
 
+#Obtain information to search for from user
+$UserName = Read-Host -Prompt 'Please enter username'
+$DaysToSearch = Read-Host -Prompt 'Please enter how many days to search prior to today'
 
-        Foreach ($Computer in $ComputerName) {
-          #Get user info
-          $UserInfo = Get-ADUser -Identity $UserName
-          #Search PDC for lockout events with ID 47
-          $LockedOutEvents = Get-WinEvent -ComputerName $Computer -FilterHashtable @{LogName='Security';Id=4740;StartTime = (Get-Date).AddDays(-$DaysToSearch)} -ErrorAction SilentlyContinue | Sort-Object -Property TimeCreated -Descending
-          #Parse and filter out lockout events
-          Foreach($Event in $LockedOutEvents)
-          {
+#endregion Variables - User Input
+
+#region Variables - Static
+
+#Gets a list of domain controllers to search through each DC
+$ComputerName = (Get-ADDomainController -Filter * |  Select-Object -ExpandProperty Name)
+
+#endregion Variables - Static
+
+#region Working Script
+
+    Foreach ($Computer in $ComputerName) {
+
+        #Get user info
+        $UserInfo = Get-ADUser -Identity $UserName
+
+        #Search PDC for lockout events with ID 47
+        $LockedOutEvents = Get-WinEvent -ComputerName $Computer -FilterHashtable @{LogName='Security';Id=4740;StartTime = (Get-Date).AddDays(-$DaysToSearch)} -ErrorAction SilentlyContinue | Sort-Object -Property TimeCreated -Descending
+
+        #Parse and filter out lockout events
+        Foreach($Event in $LockedOutEvents)
+        {
             If($Event | Where-Object {$_.Properties[2].value -match $UserInfo.SID.Value})
             {
 
-              $Event | Select-Object -Property @(
-                @{Label = 'User'; Expression = {$_.Properties[0].Value}}
-                @{Label = 'DomainController'; Expression = {$_.MachineName}}
-                @{Label = 'LockoutTimeStamp'; Expression = {$_.TimeCreated}}
-                @{Label = 'Message'; Expression = {$_.Message -split "`r" | Select-Object -First 1}}
-                @{Label = 'LockoutSource'; Expression = {$_.Properties[1].Value}}
-              )
-
-
+                $Event | Select-Object -Property @(
+                    @{Label = 'User'; Expression = {$_.Properties[0].Value}}
+                    @{Label = 'DomainController'; Expression = {$_.MachineName}}
+                    @{Label = 'LockoutTimeStamp'; Expression = {$_.TimeCreated}}
+                    @{Label = 'Message'; Expression = {$_.Message -split "`r" | Select-Object -First 1}}
+                    @{Label = 'LockoutSource'; Expression = {$_.Properties[1].Value}}
+                )
+                    
             }
-          }
-          }
+        }
+    }
+
+#endregion Working Script
